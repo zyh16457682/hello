d111548f23a28ab7ab4313305a118ec3cf4ad353
zyh <16457682@qq.com>
2017-12-04 17:37:17 +0800

    第二次测试

--- comddd/src/main/java/HelloController.java
+++ comddd/src/main/java/HelloController.java
@@ -1,10 +1,8 @@
-import org.springframework.stereotype.Controller;
 
-import javax.annotation.Resource;
-
-@Controller
 public class HelloController {
 
-    @Resource
-    private HelloService helloService;
+
+
+
+
 }
